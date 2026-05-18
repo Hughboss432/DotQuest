21fea3ce6c72b8682b3c31a5af80be9c2477968d
Hughboss432 <victorhughsilva@outlook.com>
2026-05-17 22:35:19 -0300

    fix-path_cursor

--- configs/hypr/makefile.mk
+++ configs/hypr/makefile.mk
@@ -4,8 +4,7 @@ TO := $(HOME)/.config/hypr
 hyprland-cursor:
 	rm -rf /tmp/warcraft-3 && \
 	git clone https://www.opencode.net/sorely/warcraft-3-cursors.git /tmp/warcraft-3 && \
-		cd /tmp/warcraft-3/ && \
-		cp -r /warcraft-3-cursors ~/.icons
+		cd /tmp && cp -r /warcraft-3-cursors ~/.icons
 	hyprctl setcursor warcraft-3-cursors 24
 
 hyprland-config:
